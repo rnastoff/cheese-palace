ec49e758e9da54d8a378b726e6cf1142ca521e6f
Rob Nastoff <robnastoff@gmail.com>
2024-01-06 22:58:03 -0600

    navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,16 +1,25 @@
 import Link from "next/link";
 import cheese from "@/images/cheese.svg";
+import cheesepalace from "@/images/cheesepalace.svg";
 import Image from "next/image";
 
 export default function Navbar() {
   return (
-    <header className="flex justify-between  bg-[#333333] py-8 px-2">
+    <header className="flex justify-between bg-[#333333] py-8 px-4">
       <Link href="/" className="flex">
         <Image src={cheese} width="50" height="50" alt="cheese" />
-        <h1>Cheese Palace</h1>
+        <Image
+          src={cheesepalace}
+          className="ml-2"
+          width="300"
+          height="350"
+          alt="cheese"
+        />
       </Link>
       <div className="flex">
-        <button>Login</button>
+        <button className="bg-[#fcb537] rounded-md px-8 text-white">
+          Login
+        </button>
         <div>Cart</div>
       </div>
     </header>
