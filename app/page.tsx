6016c3d822142c7aa11a2bfca69f50d67c6da409
Rob Nastoff <robnastoff@gmail.com>
2024-01-15 23:16:25 -0600

    pagination home page

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,18 @@ import { CheesePreview } from "@/types/types";
 import Slideshow from "@/components/Slideshow";
 import CheesePreviewCard from "@/components/CheesePreviewCard";
 
-async function getPreviewCheese() {
-  const query = `*[_type == 'cheese'] { 
+async function getPreviewCheese(page?: string | string[] | undefined) {
+  // We would pass this page number into a useHook
+  // search params come in as strings
+  let itemsPerPage = 10;
+  const currentPage = typeof page === "string" ? Number(page) : 1;
+  const startIndex = currentPage * itemsPerPage - itemsPerPage;
+  const endIndex = startIndex + (itemsPerPage - 1);
+  console.log("Current Page: ", currentPage);
+  console.log("Start Index: ", startIndex);
+  console.log("End Index: ", endIndex);
+
+  const query = `*[_type == 'cheese'][${startIndex}..${endIndex}] { 
         _id, 
         name, 
         sale, 
@@ -26,9 +36,32 @@ async function getSlideshow() {
   return slides;
 }
 
-export default async function Home() {
-  //Can use GROQ slicing for pagination?
-  const cheese = await getPreviewCheese();
+/*
+*Get page query parameter
+*Calculate the start index and end index 
+Page 1 = 0-9
+Page 2 = 10-19
+Page 3 = 20-29
+
+-If there is no query parameter, load 0-9
+-If there is no query parameter, load slideshow.
+-
+
+BUTTONS
+-Get full count of items 
+-Calculate number of pages -> toCeiling(totalItems / itemsPerPage)
+-Create specific number of pages <button>
+-The only thing the next/prev buttons do is push.router
+-If the number of button is currentPage, disable
+*/
+
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: { [key: string]: string | string[] | undefined };
+}) {
+  console.log("params:", searchParams.page); //remember this comes in as a string
+  const cheese = await getPreviewCheese(searchParams.page);
   const slides = await getSlideshow();
 
   const cheeseHtml = cheese.map((item: CheesePreview) => (
@@ -46,6 +79,8 @@ export default async function Home() {
     />
   ));
 
+  let currentPage = 1;
+
   return (
     <div>
       <Slideshow slides={slides} />
@@ -56,6 +91,16 @@ export default async function Home() {
       {/* Cheese grid */}
       <div className="mt-4 grid xl:grid-cols-5 lg:grid-cols-4 grid-cols-2 justify-items-center">
         {cheeseHtml}
+      </div>
+
+      {/* Next / Prev */}
+      <div className="flex justify-center mt-6">
+        <button className="border bg-gray-50 border-gray-300 w-[50px] p-4 mr-2">
+          1
+        </button>
+        <button className="border bg-gray-50 border-gray-300 w-[50px] p-4 ml-2">
+          2
+        </button>
       </div>
     </div>
   );
