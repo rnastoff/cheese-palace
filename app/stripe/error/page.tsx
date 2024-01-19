6ba1620100813924ae1748955251ca218125e318
Rob Nastoff <robnastoff@gmail.com>
2024-01-18 20:28:00 -0600

    stripe success and error pages

--- app/stripe/error/page.tsx
+++ app/stripe/error/page.tsx
@@ -1,11 +1,12 @@
 import Link from "next/link";
 
-export default function NotFound() {
+export default function Error() {
   return (
     <div className="flex flex-col items-center h-screen">
-      <h1 className="text-center w-full sm:text-5xl text-3xl font-extrabold mt-8">
-        There&apos;s no cheese here!
+      <h1 className="text-[#333333] text-center w-full sm:text-5xl text-3xl font-extrabold mt-8">
+        Something went wrong!
       </h1>
+      <p className="mt-4 text-[#333333]">There&apos;s no cheese here.</p>
       <Link
         href="/"
         className="bg-[#fcb537] text-white font-bold sm:text-2xl text-lg sm:w-[480px] w-[350px] text-center rounded-md px-4 py-2 sm:ml-4 ml-2 mt-8 active:scale-90"
--- app/stripe/success/page.tsx
+++ app/stripe/success/page.tsx
@@ -1,11 +1,18 @@
+"use client";
 import Link from "next/link";
+import { useShoppingCart } from "use-shopping-cart";
 
-export default function NotFound() {
+export default function Success() {
+  const { clearCart, cartCount } = useShoppingCart();
+
+  if ((cartCount as number) > 0) clearCart();
+
   return (
     <div className="flex flex-col items-center h-screen">
-      <h1 className="text-center w-full sm:text-5xl text-3xl font-extrabold mt-8">
-        There&apos;s no cheese here!
+      <h1 className="text-[#333333] text-center w-full sm:text-7xl text-5xl font-extrabold mt-8">
+        SUCCESS!
       </h1>
+      <p className="text-[#333333]">Thank you for your purchase.</p>
       <Link
         href="/"
         className="bg-[#fcb537] text-white font-bold sm:text-2xl text-lg sm:w-[480px] w-[350px] text-center rounded-md px-4 py-2 sm:ml-4 ml-2 mt-8 active:scale-90"
